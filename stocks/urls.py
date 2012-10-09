763ec284dda85f904ba36a8c80c61243f8d92b5a
Oscar David Arbeláez <odarbelaeze@gmail.com>
2012-10-09 15:00:41 -0500

    Stocks UI almost there.

--- stocks/urls.py
+++ stocks/urls.py
@@ -1,7 +1,28 @@
 from django.conf.urls import patterns, url
+from django.core.urlresolvers import reverse
+from django.views.generic import RedirectView
 
+from stocks.views import WareHouseList
 from stocks.views import ProductList
+from stocks.views import PriceTagList
+from stocks.views import StockList
+from stocks.views import StockEventList
+from stocks.views import WareHouseDetail
+from stocks.views import ProductDetail
+from stocks.views import PriceTagDetail
+from stocks.views import StockDetail
+from stocks.views import StockEventDetail
 
 urlpatterns = patterns('',
-    url(r'^$', ProductList.as_view(), name='product-list'),
+    url(r'^$', RedirectView.as_view(url = 'product-list')),
+    url(r'^bodegas/$', WareHouseList.as_view(), name='warehouse-list'),
+    url(r'^productos/$', ProductList.as_view(), name='product-list'),
+    url(r'^precios/$', PriceTagList.as_view(), name='pricetag-list'),
+    url(r'^existencias/$', StockList.as_view(), name='stock-list'),
+    url(r'^regexistencias/$', StockEventList.as_view(), name='stockevent-list'),
+    url(r'^bodegas/(?P<pk>\d+)/$', WareHouseDetail.as_view(), name='warehouse-detail'),
+    url(r'^productos/(?P<pk>\d+)/$', ProductDetail.as_view(), name='product-detail'),
+    url(r'^precios/(?P<pk>\d+)/$', PriceTagDetail .as_view(), name='pricetag-detail'),
+    url(r'^existencias/(?P<pk>\d+)/$', StockDetail.as_view(), name='stock-detail'),
+    url(r'^regexistencias/(?P<pk>\d+)/$', StockEventDetail.as_view(), name='stockevent-detail'),
 )
--- stocks/views.py
+++ stocks/views.py
@@ -1,14 +1,49 @@
 from django.views.generic import TemplateView
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
 
+from stocks.models import WareHouse
 from stocks.models import Product
 from stocks.models import PriceTag
+from stocks.models import Stock
+from stocks.models import StockEvent
 
 class Index(TemplateView):
-    """docstring for Index"""
     template_name = "index.html"
-        
+	
+class WareHouseList(ListView):
+	model = WareHouse
+	paginate_by = 20
 
 class ProductList(ListView):
-    model = Product
+	model = Product
+	paginate_by = 20
+
+class PriceTagList(ListView):
+	model = PriceTag
+	paginate_by = 20
+
+class StockList(ListView):
+	model = Stock
+	paginate_by = 20
+
+class StockEventList(ListView):
+	model = StockEvent
+	paginate_by = 20
+
+class WareHouseDetail(DetailView):
+	model = WareHouse
+
+class ProductDetail(DetailView):
+	model = Product
+
+class PriceTagDetail(DetailView):
+	model = PriceTag
+
+class StockDetail(DetailView):
+	model = Stock
+
+class StockEventDetail(DetailView):
+	model = StockEvent
