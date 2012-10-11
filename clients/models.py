1732ddaaec0993ee283d7388406e25de9ef3db88
Oscar David Arbeláez <odarbelaeze@gmail.com>
2012-10-11 13:31:27 -0500

    Starting clients app.

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,19 @@
+# coding=utf-8
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Route(models.Model):
+
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+    in_charge = models.OneToOneField(User)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = "Ruta"
+        verbose_name_plural = "Rutas"
+
+    def __unicode__(self):
+        return self.name
+    
+    
