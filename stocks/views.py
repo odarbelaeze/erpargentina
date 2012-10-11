d06cae7fae486ce31378f0304cbc5b3a745a76fc
Oscar David Arbeláez <odarbelaeze@gmail.com>
2012-10-11 01:41:54 -0500

    Done with the basic stocks views.

--- stocks/views.py
+++ stocks/views.py
@@ -4,6 +4,9 @@ from django.views.generic import DetailView
 from django.views.generic import UpdateView
 from django.views.generic import DeleteView
 
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 from stocks.models import WareHouse
 from stocks.models import Product
 from stocks.models import PriceTag
@@ -12,38 +15,59 @@ from stocks.models import StockEvent
 
 class Index(TemplateView):
     template_name = "index.html"
-	
+    
 class WareHouseList(ListView):
-	model = WareHouse
-	paginate_by = 15
+    model = WareHouse
+    paginate_by = 15
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(WareHouseList, self).dispatch(*args, **kwargs)
 
 class ProductList(ListView):
-	model = Product
-	paginate_by = 15
+    model = Product
+    paginate_by = 15
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ProductList, self).dispatch(*args, **kwargs)
 
 class PriceTagList(ListView):
-	model = PriceTag
-	paginate_by = 15
+    model = PriceTag
+    paginate_by = 15
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(PriceTagList, self).dispatch(*args, **kwargs)
+
 
 class StockList(ListView):
-	model = Stock
-	paginate_by = 15
+    model = Stock
+    paginate_by = 15
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(StockList, self).dispatch(*args, **kwargs)
 
 class StockEventList(ListView):
-	model = StockEvent
-	paginate_by = 15
+    model = StockEvent
+    paginate_by = 15
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(StockEventList, self).dispatch(*args, **kwargs)
 
 class WareHouseDetail(DetailView):
-	model = WareHouse
+    model = WareHouse
 
 class ProductDetail(DetailView):
-	model = Product
+    model = Product
 
 class PriceTagDetail(DetailView):
-	model = PriceTag
+    model = PriceTag
 
 class StockDetail(DetailView):
-	model = Stock
+    model = Stock
 
 class StockEventDetail(DetailView):
-	model = StockEvent
+    model = StockEvent
